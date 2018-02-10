59f3be21ed9266725107712d81add9071d24f974
Shuzhen Yu <shuzhenyu888@gmail.com>
2018-02-09 19:54:35 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -43,32 +43,25 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-#returns listing map
-@app.route("/map_1")
-def map_1():
-    return render_template("map_1.html")
-@app.route("/map_2")
-def map_2():
-    return render_template("map_2.html")
 
 # return List of neighbourhood names
 @app.route('/names')
 def names():
-
+   
     neighbourhood_results = session.query(Listings.neighbourhood).group_by(Listings.neighbourhood).all()
     neighbourhood_list = list(np.ravel(neighbourhood_results))
 
     return jsonify(neighbourhood_list)
+    
 
-
-# return List of airbnb neighborhood crime count.
+# return List of airbnb neighborhood crime count. 
 @app.route('/crimecount/<neighbourhood>')
 def crimecount(neighbourhood):
     crimecount_result=session.query(Crime.number_of_crimes).filter(Crime.neighbourhood == neighbourhood).all()
     crimecount= np.ravel(crimecount_result)
     return jsonify(int(crimecount[0]))
 
-# return List of airbnb neighborhood crime rate.
+# return List of airbnb neighborhood crime rate. 
 
 @app.route('/crimerate/<neighbourhood>')
 def crimerate(neighbourhood):
@@ -77,18 +70,18 @@ def crimerate(neighbourhood):
     crimerate= np.ravel(crimerate_result)
     return jsonify(int(crimerate[0]))
 
-# return crimeData for a given neighbourhood
+# return crimeData for a given neighbourhood  
 @app.route('/crimedata/<neighbourhood>')
 def crimedata(neighbourhood):
     crimedata = []
     crimedata_dict = {}
     crime_results = session.query(Crime.neighbourhood, Crime.number_of_crimes, Crime.crime_rate).group_by(Crime.neighbourhood).filter(Crime.neighbourhood == neighbourhood).all()
-
+    
     for result in crime_results :
         crimedata_dict["Neighbourhood"] = result[0]
         crimedata_dict["Number_of_Crimes"] = result[1]
         crimedata_dict["Crime_Rate"] = result[2]
-
+    
         crimedata.append(crimedata_dict)
 
         print(crimedata)
@@ -103,21 +96,22 @@ def entirehome(neighbourhood):
                 func.avg(Listings.availability_365)).group_by(Listings.neighbourhood).\
                 filter(Listings.room_type == 'Entire home,apt').\
                 filter(Listings.neighbourhood == neighbourhood).all()
-
+    
     Entire_home_data= []
     Entire_home_dict = {}
     for result in Entire_home_Listings_results:
-        Entire_home_dict["Neighbourhood"] = result[0]
-        Entire_home_dict["Average_Entire_Home_Price"] = result[1]
+        
+        Entire_home_dict["Average_Entire_home_Price"] = result[1]
         Entire_home_dict["Average_Number_of_Reviews"] = result[2]
         Entire_home_dict["Average_Reviews_per_Month"] = result[3]
         Entire_home_dict["Average_Availability_365"] = result[4]
 
+
         Entire_home_data.append(Entire_home_dict)
         print (Entire_home_data)
-
+    
     return jsonify(Entire_home_data)
-
+        
 
 # return Private_room information for a given neighbourhood
 @app.route('/privateroom/<neighbourhood>')
@@ -127,12 +121,12 @@ def privateroom(neighbourhood):
                 func.avg(Listings.availability_365)).group_by(Listings.neighbourhood).\
                 filter(Listings.room_type == 'Private room').\
                 filter(Listings.neighbourhood == neighbourhood).all()
-
+    
     Private_room_data= []
     Private_room_dict = {}
     for result in Private_room_Listings_results:
-
-        Private_room_dict["Neighbourhood"] = result[0]
+        
+        
         Private_room_dict["Average_Private_room_Price"] = result[1]
         Private_room_dict["Average_Number_of_Reviews"] = result[2]
         Private_room_dict["Average_Reviews_per_Month"] = result[3]
@@ -140,9 +134,9 @@ def privateroom(neighbourhood):
 
         Private_room_data.append(Private_room_dict)
         print(Private_room_data)
-
+    
     return jsonify(Private_room_data)
-
+        
 
 # return Shared_room information for a given neighbourhood
 @app.route('/sharedroom/<neighbourhood>')
@@ -152,12 +146,12 @@ def sharedroom(neighbourhood):
                 func.avg(Listings.availability_365)).group_by(Listings.neighbourhood).\
                 filter(Listings.room_type == 'Shared room').\
                 filter(Listings.neighbourhood == neighbourhood).all()
-
+    
     Shared_room_data= []
     Shared_room_dict = {}
     for result in Shared_room_Listings_results:
-
-        Shared_room_dict["Neighbourhood"] = result[0]
+        
+        
         Shared_room_dict["Average_Private_room_Price"] = result[1]
         Shared_room_dict["Average_Number_of_Reviews"] = result[2]
         Shared_room_dict["Average_Reviews_per_Month"] = result[3]
@@ -166,7 +160,7 @@ def sharedroom(neighbourhood):
         Shared_room_data.append(Shared_room_dict)
         print(Shared_room_data)
 
-
+    
     return jsonify(Shared_room_data)
 
 
@@ -190,10 +184,28 @@ def listprice(neighbourhood):
         "room_type": eachNH_df ["room_type"].values.tolist(),
         "minimum_nights": eachNH_df ["minimum_nights"].values.tolist(),
         "availability_365": eachNH_df ["availability_365"].values.tolist(),
-
+           
     }]
     print(price_data)
     return jsonify(price_data)
 
+@app.route('/correlation')
+def correlation():
+            
+    NH_compare_data = pd.read_csv("neighbourhood_compare_data.csv")
+
+    neighbourhood_compare_data = [{
+        "neighbourhood": NH_compare_data["neighbourhood"].values.tolist(),
+        "airbnb_counts_per_neighbourhood": NH_compare_data ["airbnb_counts_per_neighbourhood"].values.tolist(),
+        "total_reviews_per_neighbourhood": NH_compare_data["total_reviews_per_neighbourhood"].values.tolist(),
+        "number_of_crimes": NH_compare_data ["number_of_crimes"].values.tolist()
+
+    }]
+
+    print(neighbourhood_compare_data)
+    return jsonify(neighbourhood_compare_data)
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
